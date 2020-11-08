29fbccc658721f82df9d0de35843b0b6710e4bbf
PP <1539884184@qq.com>
2020-11-09 00:12:24 +0800

    elementui插件安装

---
+++ src/views/Login.vue
@@ -0,0 +1,70 @@
+<!--  -->
+<template>
+  <div class="login-container">
+    <div class="login-box">
+      <!-- 头像盒子 -->
+      <div class="avatar-box">
+        <img src="../assets/imgs/logo.png" alt="" />
+      </div>
+      <!-- 登录表单区 -->
+      <el-form label-width="80px">
+        <el-form-item label="活动名称">
+          <el-input></el-input>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted: {},
+
+  methods: {},
+};
+</script>
+<style lang='less' scoped>
+.login-container {
+  background-color: #2b4b6b;
+  height: 100%;
+
+  .login-box {
+    width: 450px;
+    height: 300px;
+    background-color: white;
+    border-radius: 3px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+
+    .avatar-box {
+      width: 130px;
+      height: 130px;
+      border: 1px solid #eeeeee;
+      border-radius: 50%;
+      padding: 10px;
+      box-shadow: 0 0 10px #dddddd;
+      position: absolute;
+      left: 50%;
+      top: 0;
+      transform: translate(-50%, -50%);
+      background-color: white;
+      img {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        background-color: #eeeeee;
+      }
+    }
+  }
+}
+</style>
